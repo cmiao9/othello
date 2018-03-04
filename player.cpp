8e4aaa581cc156eb22618e469c0a6c5727c0fd1a
ronayu <ronayu@caltech.edu>
2018-03-04 02:41:01 -0800

    minor edits (currently buggy)

--- player.cpp
+++ player.cpp
@@ -1,7 +1,9 @@
 #include "player.hpp"
+#include <future> 
+#include <chrono> 
 
-# Othello project: Rona Yu, Cathy Miao
-# CS2 WI 2018
+// Othello project: Rona Yu, Cathy Miao
+// CS2 WI 2018
 
 /*
  * Constructor for the player; initialize everything here. The side your AI is
@@ -9,6 +11,7 @@
  * within 30 seconds.
  */
 Player::Player(Side s) {
+	time_t timer1 = time(0);
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
 
@@ -19,10 +22,15 @@ Player::Player(Side s) {
      */
      
      // create a new board object 
-     Board * board = new Board();
+     Board board = new Board();
      
      // set your (the player's) side 
      Side side = s; 
+     
+     time_t timer2 = time(0);
+     if((timer2-timer1)>30){
+		 cerr << "Timout in player construction" << endl; 
+	 }
 }
 
 /*
@@ -50,12 +58,28 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * process the opponent's opponents move before calculating your own move
      */
      
-     // see if the board has a move
-     if(board.hasMoves(side) == false){
-		 return nullptr;
+     if(msLeft == -1){
+		 // no time limit
 	 }
+	 
 	 else{
-		 // process the opponent's move
-		 // calculate your own move
-	 }
+		 // time limit
+		 time_t timer1 = time(0); // get the current time 
+
+		// see if the board has a move
+		if(this->board.hasMoves(this->side) == false){
+			return nullptr;
+		}
+		else{
+			// process the opponent's move
+			// calculate your own move
+		}
+		
+		// // after everything's done 
+		time_t timer2 = time(0); 
+		if((timer1 - timer2)>=(msLeft)/1000){
+			cerr << "Timeout Error in doMove" << endl; 
+		}
+			
+	}
 }
