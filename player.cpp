a34249b6fbef1cefd448814bb2f1bf8e66ac14bf
ronayu <ronayu@caltech.edu>
2018-03-03 19:42:21 -0800

    first update

--- player.cpp
+++ player.cpp
@@ -1,5 +1,8 @@
 #include "player.hpp"
 
+# Othello project: Rona Yu, Cathy Miao
+# CS2 WI 2018
+
 /*
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
